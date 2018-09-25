6bec02ac4381eca1be67040ad9fa2fa8b844ed3a
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-09-25 13:07:33 -0400

    fixed pop_up

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 
+window.onload = function () {
+  /* Cache the popup. */
+  var popup = document.getElementById("popup");
+  
+  /* Show the popup. */
+  popup.classList.remove("hidden");
+  
+  /* Fade the popup in */
+  setTimeout(()=>popup.classList.add("fade-in"));
 
-const name = window.prompt("Please enter your name:")
-const ageCheck = window.prompt("Please enter your age:")
 
-if (ageCheck < 21) {
-	window.alert("Thanks you for visiting " + name + ", but the legal drinking age limit is 21 and over, and you entered " + ageCheck + ".");
-	window.alert("Please vist the site again when you're 21.");
-} else if (ageCheck == 21 || ageCheck > 21 && ageCheck < 123){
-	window.alert("Welcome, " + name + "!");
+  // /* Close the popup when a age is selected. */
+  // document.getElementById("age").onchange = function () {
+
+
+  //     Fade the popup out 
+  //    popup.classList.remove("fade-in");
+     
+  //    /* Hide the popup. */
+  //    setTimeout(()=>popup.classList.add("hidden"), 300);
+  // };
+};
+
+
+
+ function myFunction(){
+ let x;
+  x = document.getElementById('age').value;
+
+if ( x < 21 && x > 0){
+	window.alert('You submitted the age of ' + x +'.  Please come back when you are of legal drinking age.');
+	text = 'Farewell';
+}else if (x >= 21 && x <= 123){
+	window.alert('Welcome in!');
 	window.location.href = "main.html";
-} else if (ageCheck > 123){
-	window.alert("The oldest recorded human was Jeanne Calmet, who died at the age 122,on the year of 1997.  You are most likely NOT older than Jeanne.")
+}else if (x > 123){
+    window.alert('The oldest recorded human was Jeanne Calmet, who died at the age 122, on the year of 1997.  You are most likely NOT older than Jeanne.');
+    text = 'Good Bye';
+}else {
+	window.alert('Plese enter a human age.');
+	text = 'Invalid Input';
 }
-else {
-	window.alert("Please enter a human age.")
-}
+
+    document.getElementById("greeting_text").innerHTML = text;
+};
