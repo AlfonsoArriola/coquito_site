dde269285ec9f9dc259ee1ee4708b616c34e447e
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-10-02 03:21:53 -0400

    commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 window.onload = function () {
 
-  // for slider on ad
-  printValue('age2', 'rangeValue');
-  /* Cache the popup. */
   let popup = document.getElementById("popup");
   
   /* Show the popup. */
@@ -11,7 +8,6 @@ window.onload = function () {
   
   /* Fade the popup in */
   setTimeout(()=>popup.classList.add("fade-in"));
-
 
   // /* Close the popup when a age is selected. */
   // document.getElementById("age").onchange = function () {
@@ -62,6 +58,16 @@ function slimScrollGo() {
 // =================++++++  Ad  ++++++======================
 
 $(document).ready(function(){
+  //Check for local storage;
+  //If it exisits, set variable and hide form
+   let storedPampering = localStorage.getItem('pamper'); 
+   if(storedPampering != null){
+        $('#SpecialOffer').removeClass('unsubmitted');
+        $('.detail').css('display', 'none');
+   }
+   //If form unsubmitted, do card flip
+   if($('#specialOffer').hasClass('unsubmitted')){
+
 
     $('.cardGroup.flip').hover(
         function () {
@@ -71,6 +77,10 @@ $(document).ready(function(){
           $(this).find('.card').removeClass('theFlip');     
         }
       );
+    }
+
+// Draw Initial Canvas
+if(storedPampering == null){
 
   $('#myCanvas').drawText({
     layer: true,
@@ -81,6 +91,41 @@ $(document).ready(function(){
     name: 'startLayer',
     maxWidth: 200
   });
+} else{
+  let theText;
+  let theImage;
+  if(storedPampering == 'tan'){
+    theImage = 'images/tanning_bed.jpeg';
+  } else {
+    theImage = 'images/monkeys.jpeg';
+  }
+
+  $('#myCanvas').addLayer({
+    type: 'image',
+    source: theImage,
+    x:100, y:100,
+    load:function(){
+      $(this).drawLayer(1);
+      $(this).drawLayer(2);
+    }
+  });
+
+  $('#myCanvas').addLayer({
+     type: 'text',
+     fillStyle: '#fff',
+     font: 'bold 32pt Tangerine',
+     x:101, y:101,
+     maxWidth: 200
+  });
+  $('#myCanvas').addLayer({
+     type: 'text',
+     fillStyle: '#36c',
+     font: 'bold 32pt Tangerine',
+     x:100, y:100,
+     maxWidth: 200
+  });
+  $('#myCanvas').drawLayer();
+}
   // form submit buttons
   $('#theButton').click(function(){
     let theAge2 = $('#age2').val();
@@ -91,8 +136,8 @@ $(document).ready(function(){
   })
 
   //Get items from local storage
-  let storedAge2 = localStorage.getItem('age2');
-  let storedPampering = localStorage.getItem('pamper');
+  // let storedAge2 = localStorage.getItem('age2');
+  // let storedPampering = localStorage.getItem('pamper');
    let theAction;
    let theImage;
    let theImageAlt;
@@ -113,13 +158,7 @@ $(document).ready(function(){
 
 });
 
-// -----------  Update text field for slider   
 
-function printValue(sliderID, textbox) {
-  let x = document.getElementById(textbox);
-  let y = document.getElementById(sliderID);
-  x.value = y.value;
-}
 
 
 
